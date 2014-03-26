50dfca1a6393f24ee36b5989de85c4d0c398e8d1
quentin <quentin@vernot.net>
2014-03-26 22:14:56 +0100

    removed the need to indicate the server's port

--- main.cpp
+++ main.cpp
@@ -3,17 +3,19 @@
 
 int main(int argc, char *argv[]){
 
-	if(argc <= 1 || argc == 3){
-		std::cout << "USAGE: main <nickname> [<server address> <server port>]" << std::endl;
+	if(argc > 3){
+		std::cout << "USAGE: " << argv[0] << " [<nickname> <server address>]" << std::endl;
 		return 1;
 	}
 
 	try{
 		Game *game;
-		if(argc == 2)
+		if(argc == 1)
+			game = new Game();
+		else if(argc == 2)
 			game = new Game(argv[1]);
 		else
-			game = new Game(argv[1], argv[2], argv[3]);
+			game = new Game(argv[1], argv[2]);
 		game->go();
 		delete game;
 	}
